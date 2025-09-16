052655789543f25df06f7c5774039d1d507134db
2dam <2dam@info.quevedo.local>
2025-09-16 10:27:45 +0200

    Initial commit

---
+++ src/main/java/newspaperoot/dao/CredentialRepository.java
@@ -0,0 +1,8 @@
+package newspaperoot.dao;
+//pregunta las credenciales al usuario
+//y las compara con las credenciales almacenadas en la base de datos
+//si son correctas, permite el acceso al sistema
+//si no, muestra un mensaje de error
+//y vuelve a pedir las credenciales
+public class CredentialRepository {
+}
---
+++ src/main/java/newspaperoot/ui/mainMenu.java
@@ -0,0 +1,69 @@
+package newspaperoot.ui;
+
+import java.util.Scanner;
+
+public class mainMenu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Username");
+            String username = sc.nextLine();
+            System.out.println("Password");
+            String password = sc.nextLine();
+            if (username.equals("root") && password.equals("root")) {
+                System.out.println("Login successful. Welcome " + username + "!");
+                System.out.println("Elije una opcion:");
+                int opc = sc.nextInt();
+                do {
+                switch (opc) {
+                    case 1: {
+                    System.out.println("1. Get all articles");}
+                    break;
+                    case 2: {
+                    System.out.println("2. Add article");}
+                    break;
+                    case 3: {
+                    System.out.println("3. Update article");}
+                    break;
+                    case 4: {
+                    System.out.println("4. Delete article");}
+                    break;
+                    case 5: {
+                    System.out.println("5. Get all Newspapers");}
+                    break;
+                    case 6: {
+                    System.out.println("6. Get all readers");}
+                    break;
+                    case 7: {
+                    System.out.println("7. Get Readers of an Article");}
+                    break;
+                    case 8: {
+                    System.out.println("8. Get reader by id");}
+                    break;
+                    case 9: {
+                    System.out.println("9. Add rating to an Article");}
+                    break;
+                    case 10: {
+                    System.out.println("10. Modify rating of an Article");}
+                    break;
+                    case 11: {
+                    System.out.println("11. Delete rating of an Article");}
+                    break;
+                    case 12: {
+                    System.out.println("12. Get all types");}
+                    break;
+                    case 13: {
+                    System.out.println("13. Exit");}
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                        break;
+                }
+                } while (opc != 13);
+            } else {
+                System.out.println("Login failed. Incorrect username or password.");
+            }
+
+        sc.close();
+
+
+    }
+}
